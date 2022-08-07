c3e964855e6d22cd84c49ef7a0e0d9cd6c7e0651
ByQush <mehmettaskn@yahoo.com>
2022-08-07 12:49:20 +0300

    0.0.1

---
+++ Settings/bot.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bot: {
+        TOKEN : "ODQ5MTcyMzM3MDkzNzcxMjY1.G6Q9Cu.7LucJM3r6myTH1W3xCC9jJav9DCbipk8_bHu38",
+        ID: "849172337093771265"
+    },
+
+    supportServer: {
+        ID: "718835209948233778"
+    }
+};
