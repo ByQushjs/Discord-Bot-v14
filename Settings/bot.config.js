7c120140241dff2f45e8dc499b1a7f2086bf72c0
ByQush <mehmettaskn@yahoo.com>
2022-08-31 01:11:39 +0300

    Update bot.config.js

--- Settings/bot.config.js
+++ Settings/bot.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     bot: {
-        TOKEN : "ODQ5MTcyMzM3MDkzNzcxMjY1.G6Q9Cu.7LucJM3r6myTH1W3xCC9jJav9DCbipk8_bHu38",
-        ID: "849172337093771265"
+        TOKEN : "",
+        ID: ""
     },
 
     supportServer: {
-        ID: "718835209948233778"
+        ID: ""
     }
 };
